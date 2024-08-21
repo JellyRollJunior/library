902cdff02f0a4e815b061651d0a6c589dd618613
Brandon Lin <brandonlin9716@gmail.com>
2024-08-20 21:06:39 -0700

    Book cards will display info for each book

--- javascript.js
+++ javascript.js
@@ -7,11 +7,13 @@ function Book(title, author, pages, haveRead) {
     this.haveRead = haveRead;
 }
 
-Book.prototype.info = function() {
-    let bookInformation = `${this.title} by ${this.author}, ${this.pages} pages, `
-    bookInformation = this.haveRead ? bookInformation + "have read" : bookInformation + "not read yet";
-    return  bookInformation;
-}
+Book.prototype.info = function () {
+    let bookInformation = `${this.title} by ${this.author}, ${this.pages} pages, `;
+    bookInformation = this.haveRead
+        ? bookInformation + "have read"
+        : bookInformation + "not read yet";
+    return bookInformation;
+};
 
 function addBookToLibrary(book) {
     for (const book of arguments) {
@@ -21,15 +23,24 @@ function addBookToLibrary(book) {
 
 function displayLibraryBooks() {
     for (const book of myLibrary) {
-        let bookCardTemplate = document.querySelector(".card.template").cloneNode(true);
+        let bookCardTemplate = document
+            .querySelector(".card.template")
+            .cloneNode(true);
+        bookCardTemplate.querySelector(".title").textContent = book.title;
+        bookCardTemplate.querySelector(".author").textContent = book.author;
+        bookCardTemplate.querySelector(".page-count").textContent = book.pages;
+        if (book.haveRead) {
+            bookCardTemplate.querySelector(".have-read").textContent =
+                "Have read";
+        }
         document.querySelector(".content").appendChild(bookCardTemplate);
     }
 }
 
 // Test data
-let book1 = new Book("Test 1", "author 1", 250, true);
-let book2 = new Book("Test 2", "author 2", 250, false);
-let book3 = new Book("Test 3", "author 3", 250, true);
+let book1 = new Book("Bao bao number 1", "Me", 5000, true);
+let book2 = new Book("u uwawa uwa", "Chiikawa", 250, false);
+let book3 = new Book("yaha ururururur", "usagi", 450, true);
 
 // execution phase
 addBookToLibrary(book1, book2, book3);
